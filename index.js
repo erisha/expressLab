dcadc1907cb2dcd62d788eb51b6bfc1d61d7aff8
Erisha Roach <forever.erisha@gmail.com>
2024-01-04 15:56:42 -0500

    added dependencies. connected to server. created main route. added morgan middleware

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+//middleware
+const morgan = require('morgan')
+app.use(morgan)
+
+//main route
+app.get('/', (req,res) => {
+    console.log(req.url);
+    res.send('<h1>Homepage</h1>')
+})
